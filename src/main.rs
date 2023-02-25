183e1c94f7853602f44a950f50008e5b282f3262
Andy Gayton <andy@thecablelounge.com>
2023-02-25 12:35:41 -0500

    add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::BufRead;
+use std::io::Write;
+
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(value_parser)]
+    command: String,
+    #[clap(value_parser)]
+    args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let mut child = std::process::Command::new(&args.command)
+            .args(&args.args)
+            .stdin(std::process::Stdio::piped())
+            .spawn()
+            .unwrap();
+        {
+            let mut stdin = child.stdin.take().unwrap();
+            stdin.write_all(line.as_bytes()).unwrap();
+        }
+    }
 }
