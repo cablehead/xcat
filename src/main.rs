5b0cdf0fe906599c40fa9b14eac9c09a5f4fa5b5
Andy Gayton <andy@thecablelounge.com>
2023-04-20 18:14:23 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::BufRead;
 use std::io::Write;
 
 use clap::Parser;
+use tiktoken_rs::cl100k_base;
 
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
@@ -10,24 +11,32 @@ struct Args {
     command: String,
     #[clap(value_parser)]
     args: Vec<String>,
+    #[clap(long, value_parser)]
+    tiktoken: Option<usize>,
 }
 
 fn main() {
     let args = Args::parse();
     let stdin = std::io::stdin();
 
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
+    let bpe = cl100k_base().unwrap();
 
-        let mut p = std::process::Command::new(&args.command)
-            .args(&args.args)
-            .stdin(std::process::Stdio::piped())
-            .spawn()
-            .unwrap();
+    if let Some(token_limit) = args.tiktoken {
+        // Handle tiktoken option
+    } else {
+        for line in stdin.lock().lines() {
+            let line = line.unwrap();
 
-        let mut stdin = p.stdin.take().unwrap();
-        writeln!(stdin, "{}", line).unwrap();
-        drop(stdin);
-        p.wait().unwrap();
+            let mut p = std::process::Command::new(&args.command)
+                .args(&args.args)
+                .stdin(std::process::Stdio::piped())
+                .spawn()
+                .unwrap();
+
+            let mut stdin = p.stdin.take().unwrap();
+            writeln!(stdin, "{}", line).unwrap();
+            drop(stdin);
+            p.wait().unwrap();
+        }
     }
 }
