66d0582e562107d5e53d3889ed60d5a8048a036e
Andy Gayton <andy@thecablelounge.com>
2023-04-21 10:19:19 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{BufRead, BufReader, Write};
+use std::iter::Iterator;
 
 use clap::Parser;
 use tiktoken_rs::cl100k_base;
@@ -32,30 +33,36 @@ fn main() {
     let args = Args::parse();
     let stdin = std::io::stdin();
 
-    if let Some(token_limit) = args.tiktoken {
+    let iter: Box<dyn Iterator<Item = String>> = if let Some(token_limit) = args.tiktoken {
         let bpe = cl100k_base().unwrap();
         let mut reader = BufReader::new(stdin.lock());
         let mut buffer = String::new();
         let mut tokens = Vec::new();
 
-        while reader.read_line(&mut buffer).unwrap() > 0 {
-            let new_tokens = bpe.encode_with_special_tokens(&buffer);
-            tokens.extend(new_tokens);
-            buffer.clear();
+        Box::new(std::iter::from_fn(move || {
+            while reader.read_line(&mut buffer).unwrap() > 0 {
+                let new_tokens = bpe.encode_with_special_tokens(&buffer);
+                tokens.extend(new_tokens);
+                buffer.clear();
 
-            while tokens.len() >= token_limit {
-                let chunk = bpe.decode(tokens[..token_limit].to_vec()).unwrap();
-                tokens.drain(..token_limit);
-                spawn_command(&args, vec![chunk]);
+                if tokens.len() >= token_limit {
+                    let chunk = bpe.decode(tokens[..token_limit].to_vec()).unwrap();
+                    tokens.drain(..token_limit);
+                    return Some(chunk);
+                }
             }
-        }
 
-        if !tokens.is_empty() {
-            let chunk = bpe.decode(tokens).unwrap();
-            spawn_command(&args, vec![chunk]);
-        }
+            if !tokens.is_empty() {
+                let chunk = bpe.decode(tokens).unwrap();
+                tokens.clear();
+                return Some(chunk);
+            }
+
+            None
+        }))
     } else {
-        let lines = stdin.lock().lines().map(|line| line.unwrap());
-        spawn_command(&args, lines);
-    }
+        Box::new(stdin.lock().lines().map(|line| line.unwrap()))
+    };
+
+    spawn_command(&args, iter);
 }
